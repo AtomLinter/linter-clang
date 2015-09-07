9288c14e6c841eee94bb2ee24452b75539934bbb
Kepler Sticka-Jones <kepler+git@stickajones.org>
2015-09-06 21:56:02 -0600

    Add fixtures for a simple missing import test.
    Taken from k2b6s9j/lint-checks

---
+++ spec/fixtures/missing_import.m
@@ -0,0 +1,7 @@
+// This is a comment is will not return an error
+#include "does_not_exist.h"
+
+int main(int argc, char const *argv[]) {
+  /* code */
+  return 0;
+}
---
+++ spec/fixtures/missing_import.mm
@@ -0,0 +1,7 @@
+//This is a comment, it will not return an error.
+#import "does_not_exist.h"
+
+int main(int argc, char const *argv[]) {
+  /* code */
+  return 0;
+}
