a2ad040724673254f86513b30abb93e22468e4b4
Kepler Sticka-Jones <kepler+git@stickajones.org>
2015-09-06 22:46:45 -0600

    while I pondered weak and weary

--- spec/linter-clang-spec.js
+++ spec/linter-clang-spec.js
@@ -4,7 +4,7 @@ describe("The Clang Provider for AtomLinter", () => {
 
   beforeEach(() => {
     provider = require("../lib/main").provideLinter();
-    
+
     waitsForPromise(() => {
       return atom.packages.activatePackage("linter-clang")
     });
@@ -15,30 +15,6 @@ describe("The Clang Provider for AtomLinter", () => {
       waitsForPromise(() => {
         return atom.workspace.open("./files/missing_import.c").then((editor) => {
           console.log(editor);
-        });
-      });
-    });
-
-    describe("in 'missing_import.cpp'", () => {
-      beforeEach(() => {
-        waitsForPromise(() => {
-          atom.workspace.open("missing_import.cpp");
-        });
-      });
-    });
-
-    describe("in 'missing_import.m'", () => {
-      beforeEach(() => {
-        waitsForPromise(() => {
-          atom.workspace.open("missing_import.m");
-        });
-      });
-    });
-
-    describe("in 'missing_import.mm'", () => {
-      beforeEach(() => {
-        waitsForPromise(() => {
-          atom.workspace.open("missing_import.mm");
         });
       });
     });
