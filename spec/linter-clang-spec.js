2faf78fc7fad4f4a3fed523e47dea848fc3b4b77
Kepler Sticka-Jones <kepler+git@stickajones.org>
2015-09-06 22:14:44 -0600

    Actually loading the files.
    Goddamn it.

--- spec/linter-clang-spec.js
+++ spec/linter-clang-spec.js
@@ -9,26 +9,34 @@ describe("The Clang Provider for AtomLinter", () => {
 
   describe("finds issue with the code", () => {
     describe("in 'missing_import.c'", () => {
-      waitsForPromise(() => {
-        atom.workspace.open("missing_import.c");
+      beforeEach(() => {
+        waitsForPromise(() => {
+          atom.workspace.open("missing_import.c");
+        });
       });
     });
 
     describe("in 'missing_import.cpp'", () => {
-      waitsForPromise(() => {
-        atom.workspace.open("missing_import.cpp");
+      beforeEach(() => {
+        waitsForPromise(() => {
+          atom.workspace.open("missing_import.cpp");
+        });
       });
     });
 
     describe("in 'missing_import.m'", () => {
-      waitsForPromise(() => {
-        atom.workspace.open("missing_import.m");
+      beforeEach(() => {
+        waitsForPromise(() => {
+          atom.workspace.open("missing_import.m");
+        });
       });
     });
 
     describe("in 'missing_import.mm'", () => {
-      waitsForPromise(() => {
-        atom.workspace.open("missing_import.mm");
+      beforeEach(() => {
+        waitsForPromise(() => {
+          atom.workspace.open("missing_import.mm");
+        });
       });
     });
   });
