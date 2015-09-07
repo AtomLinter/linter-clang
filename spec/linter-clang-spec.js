939826ee88ec8f4abedc7a11016d00ba527c7dc2
Kepler Sticka-Jones <kepler+git@stickajones.org>
2015-09-06 23:24:41 -0600

    I mean this is a step closer...

--- spec/linter-clang-spec.js
+++ spec/linter-clang-spec.js
@@ -1,14 +1,15 @@
-'use babel'
+"use babel";
 
 describe('The Clang provider for AtomLinter', () => {
-  const provider = require('../lib/main').provideLinter()
-  describe('finds issue with the code', () => {
-    it('works in "missing_import.c"', () => {
-      waitsForPromise(() =>
-        atom.workspace.open(__dirname + '/files/missing_import.c').then(editor => {
-          console.log(editor)
+  const lint = require('../lib/main').provideLinter().lint
+
+  it('finds a fatal error in "missing_import.c"', () => {
+    waitsForPromise(() => {
+      return atom.workspace.open(__dirname + '/files/missing_import.c').then(editor => {
+        return lint(editor).then(messages => {
+          console.log(messages);
         })
-      )
+      })
     })
   })
 })
