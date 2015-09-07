42c6ad5f110bc9e1ae68f26c6b46ea7513593f51
Kepler Sticka-Jones <kepler+git@stickajones.org>
2015-09-06 22:44:27 -0600

    Once upon a midnight dreary.

--- spec/linter-clang-spec.js
+++ spec/linter-clang-spec.js
@@ -1,22 +1,20 @@
 "use babel";
 
-provider = require("../lib/main").provideLinter();
-
 describe("The Clang Provider for AtomLinter", () => {
+
   beforeEach(() => {
+    provider = require("../lib/main").provideLinter();
+    
     waitsForPromise(() => {
-      atom.packages.activatePackage("linter-clang");
+      return atom.packages.activatePackage("linter-clang")
     });
   });
 
   describe("finds issue with the code", () => {
-    it("in 'missing_import.c'", () => {
+    describe("in 'missing_import.c'", () => {
       waitsForPromise(() => {
-        atom.workspace.open("./files/missing_import.c").then((editor) => {
-          provider.lint(editor).then((messages) => {
-            expect(messages.length).toEqual(0);
-            expect(messages[0].text).toEqual("'nothing.h' file not found");
-          });
+        return atom.workspace.open("./files/missing_import.c").then((editor) => {
+          console.log(editor);
         });
       });
     });
