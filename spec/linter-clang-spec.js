7d7c87918a55d48e7590dbc7a9b7ebd956b396fc
Kepler Sticka-Jones <kepler+git@stickajones.org>
2015-09-06 23:28:35 -0600

    Load the package.

--- spec/linter-clang-spec.js
+++ spec/linter-clang-spec.js
@@ -3,6 +3,12 @@
 describe('The Clang provider for AtomLinter', () => {
   const lint = require('../lib/main').provideLinter().lint
 
+  beforeEach(() => {
+    waitsForPromise(() => {
+      return atom.packages.activatePackage("linter-clang")
+    })
+  })
+
   it('finds a fatal error in "missing_import.c"', () => {
     waitsForPromise(() => {
       return atom.workspace.open(__dirname + '/files/missing_import.c').then(editor => {
