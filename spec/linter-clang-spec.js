ad82e97f3e48236ced37b9c292d248ffd3bf084d
Landon Abney <Landonabney@gmail.com>
2017-03-30 15:24:29 -0700

    Use async/await in specs
    Requires the `jasmine-fix` module to get them working.

--- spec/linter-clang-spec.js
+++ spec/linter-clang-spec.js
@@ -1,64 +1,57 @@
 'use babel';
 
-import * as path from 'path';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { beforeEach, it } from 'jasmine-fix';
+import { join } from 'path';
 
-const miPath = path.join(__dirname, 'files', 'missing_import');
-const validPath = path.join(__dirname, 'files', 'valid.c');
+const lint = require('../lib/main').provideLinter().lint;
+
+const miPath = join(__dirname, 'files', 'missing_import');
+const validPath = join(__dirname, 'files', 'valid.c');
 
 describe('The Clang provider for AtomLinter', () => {
-  const lint = require('../lib/main').provideLinter().lint;
-
-  beforeEach(() => {
-    waitsForPromise(() => atom.packages.activatePackage('linter-clang'));
+  beforeEach(async () => {
+    await atom.packages.activatePackage('language-c');
+    await atom.packages.activatePackage('linter-clang');
   });
 
-  it('finds nothing wrong with a valid file', () => {
-    waitsForPromise(() =>
-      atom.workspace.open(validPath).then(editor =>
-        lint(editor).then((messages) => {
-          expect(messages.length).toBe(0);
-        }),
-      ),
-    );
+  it('finds nothing wrong with a valid file', async () => {
+    const editor = await atom.workspace.open(validPath);
+    const messages = await lint(editor);
+    expect(messages.length).toBe(0);
   });
 
-  it('finds a fatal error in "missing_import.c"', () => {
-    waitsForPromise(() => atom.workspace.open(`${miPath}.c`).then(
-      editor => lint(editor).then((messages) => {
-        expect(messages.length).toEqual(1);
-        expect(messages[0].type).toEqual('fatal error');
-        expect(messages[0].text).toEqual("'nothing.h' file not found");
-      }),
-    ));
-  });
+  describe('handles errors in different file types', () => {
+    it('finds a fatal error in "missing_import.c"', async () => {
+      const editor = await atom.workspace.open(`${miPath}.c`);
+      const messages = await lint(editor);
+      expect(messages.length).toBe(1);
+      expect(messages[0].type).toBe('fatal error');
+      expect(messages[0].text).toBe("'nothing.h' file not found");
+    });
 
-  it('finds a fatal error in "missing_import.cpp"', () => {
-    waitsForPromise(() => atom.workspace.open(`${miPath}.cpp`).then(
-      editor => lint(editor).then((messages) => {
-        expect(messages.length).toEqual(1);
-        expect(messages[0].type).toEqual('fatal error');
-        expect(messages[0].text).toEqual("'nothing.h' file not found");
-      }),
-    ));
-  });
+    it('finds a fatal error in "missing_import.cpp"', async () => {
+      const editor = await atom.workspace.open(`${miPath}.cpp`);
+      const messages = await lint(editor);
+      expect(messages.length).toBe(1);
+      expect(messages[0].type).toEqual('fatal error');
+      expect(messages[0].text).toEqual("'nothing.h' file not found");
+    });
 
-  it('finds a fatal error in "missing_import.m"', () => {
-    waitsForPromise(() => atom.workspace.open(`${miPath}.m`).then(
-      editor => lint(editor).then((messages) => {
-        expect(messages.length).toEqual(1);
-        expect(messages[0].type).toEqual('fatal error');
-        expect(messages[0].text).toEqual("'nothing.h' file not found");
-      }),
-    ));
-  });
+    it('finds a fatal error in "missing_import.m"', async () => {
+      const editor = await atom.workspace.open(`${miPath}.m`);
+      const messages = await lint(editor);
+      expect(messages.length).toBe(1);
+      expect(messages[0].type).toEqual('fatal error');
+      expect(messages[0].text).toEqual("'nothing.h' file not found");
+    });
 
-  it('finds a fatal error in "missing_import.mm"', () => {
-    waitsForPromise(() => atom.workspace.open(`${miPath}.mm`).then(
-      editor => lint(editor).then((messages) => {
-        expect(messages.length).toEqual(1);
-        expect(messages[0].type).toEqual('fatal error');
-        expect(messages[0].text).toEqual("'nothing.h' file not found");
-      }),
-    ));
+    it('finds a fatal error in "missing_import.mm"', async () => {
+      const editor = await atom.workspace.open(`${miPath}.mm`);
+      const messages = await lint(editor);
+      expect(messages.length).toBe(1);
+      expect(messages[0].type).toEqual('fatal error');
+      expect(messages[0].text).toEqual("'nothing.h' file not found");
+    });
   });
 });
